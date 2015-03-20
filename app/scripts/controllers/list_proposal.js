5c22a2847ab39592d5ddb803f037fcc3aea991bb
Maksymjs <maxdevelopenko@gmail.com>
2015-03-20 15:28:49 +0200

    UI with choosing certain fields.

--- app/scripts/controllers/list_proposal.js
+++ app/scripts/controllers/list_proposal.js
@@ -14,4 +14,18 @@ angular.module('admissionSystemApp')
       'AngularJS',
       'Karma'
     ];
+  })
+
+  .controller('mainTable', function($scope){
+    $scope.headers = ['Напрям', 'Назва', 'Форма', 'Орк','Структурний підрозділ', 'Ліц.обсяг', 'Держю замовлення','Термю навч.', 'Дата початку'];
+    //$scope.rows = ['6.44.3242', 'Філологія', 'Денна', 'Бакалавр','Факультет іноземних мов', '43', '0','4', '01.09.2015'];
+    $scope.rows = [
+      {direct: '6.44.3242', name: 'Філологія', forma: "Денна", ork: "Бакалавр", struc: 'Факультет іноземних мов',lic: '43', state: '0', period: "4",data: "01.09.2015"},
+      {direct: '6.44.3242', name: 'Філологія', forma: "Денна", ork: "Бакалавр", struc: 'Факультет іноземних мов',lic: '43', state: '0', period: "4",data: "01.09.2015"}
+    ];
+
+    $scope.show = {};
+    for(var key in $scope.headers){
+      $scope.show[key] = true;
+    }
   });
