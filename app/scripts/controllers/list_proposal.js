f910f4d872dedf380d9c827d9b829b46f6a1a578
Maksymjs <maxdevelopenko@gmail.com>
2015-03-27 00:00:20 +0200

    possibility to choose certain fields

--- app/scripts/controllers/list_proposal.js
+++ app/scripts/controllers/list_proposal.js
@@ -8,15 +8,33 @@
  * Controller of the admissionSystemApp
  */
 angular.module('admissionSystemApp')
-  .controller('ListProposalCtrl', ['$scope','ngTableParams', 'ListProposalGettingService', function ($scope, ngTableParams, ListProposalGettingService) {
+  .controller('ListProposalCtrl', ['$scope','$filter', 'ngTableParams', 'ListProposalGettingService', function ($scope, $filter, ngTableParams, ListProposalGettingService) {
+
+    $scope.headers = [
+      {name: "id", display: "id", visible: true},
+      {name: "specialtyId", display: "speciality", visible: true},
+      {name: "departmentId", display: "department", visible: true},
+      {name: "timePeriodId", display: "timePeriod", visible: true},
+      {name: "timePeriodCourseId", display: "timePeriodCarousel", visible: true},
+      {name: "specofferTypeId", display: "specofferType", visible: true},
+      {name: "eduFormTypeId", display: "eduFormType", visible: true},
+      {name: "licCount", display: "licCount", visible: true},
+      {name: "stateCount", display: "stateCount", visible: true}
+    ]
 
     ListProposalGettingService.getAllProposals(8).then(function (data) {
       $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
-        count: 10           // count per page
+        count: 10,           // count per page
+        filter: {
+          name: 'M'       // initial filter
+        }
       }, {
         total: data.length, // length of data
         getData: function ($defer, params) {
+          var orderedData = params.sorting() ?
+            $filter('orderBy')(data, params.orderBy()) :
+            data;
           $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
       });
