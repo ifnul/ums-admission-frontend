988a006be19f641da4c2512abe6d97b2aa9b6925
Andrey Vasylenko <0933322888@ukr.net>
2015-04-01 19:43:47 +0300

    Main table headers rename

--- app/scripts/controllers/list_proposal.js
+++ app/scripts/controllers/list_proposal.js
@@ -11,14 +11,14 @@ angular.module('admissionSystemApp')
   .controller('ListProposalCtrl', ['$scope','$filter','ngTableParams', 'ListProposalGettingService', '$modal', function ($scope, $filter, ngTableParams, ListProposalGettingService, $modal) {
 
     $scope.headers = [
-  +      {name: "id", display: "№", visible: true},
-+      {name: "specialtyId", display: "Спеціальність", visible: true},
-+      {name: "departmentId", display: "Структурний підрозділ", visible: true},
-+      {name: "timePeriodCourseId", display: "Курс зарахування", visible: true},
-+      {name: "specofferTypeId", display: "Тип пропозиції", visible: true},
-+      {name: "eduFormTypeId", display: "Форма навчання", visible: true},
-+      {name: "licCount", display: "Ліцензований обсяг", visible: true},
-+      {name: "stateCount", display: "Державне замовлення", visible: true}
+        {name: "id", display: "№", visible: true},
+      {name: "specialtyId", display: "Спеціальність", visible: true},
+      {name: "departmentId", display: "Структурний підрозділ", visible: true},
+      {name: "timePeriodCourseId", display: "Курс зарахування", visible: true},
+      {name: "specofferTypeId", display: "Тип пропозиції", visible: true},
+      {name: "eduFormTypeId", display: "Форма навчання", visible: true},
+      {name: "licCount", display: "Ліцензований обсяг", visible: true},
+      {name: "stateCount", display: "Державне замовлення", visible: true}
      ];
 
     $scope.openFiltersModal = function (size) {
