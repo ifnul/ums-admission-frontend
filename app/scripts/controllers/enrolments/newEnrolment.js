1e41c728b3962392667b1597af2b2bf7d334b6a8
Daria Gorchylina <daria.gorchylina@gmail.com>
2015-05-13 01:42:01 +0300

    adding rest angular service for enrolment

--- app/scripts/controllers/enrolments/newEnrolment.js
+++ app/scripts/controllers/enrolments/newEnrolment.js
@@ -2,10 +2,15 @@
 
 angular
   .module('admissionSystemApp')
-  .controller('NewEnrolmentCtrl', ['$scope', '$stateParams', 'baseFormData', '$state',
-    function ($scope, $stateParams, baseFormData, $state) {
+  .controller('NewEnrolmentCtrl', ['$scope', '$stateParams', 'baseFormData', '$state', 'EnrolmentService',
+    'DictionariesSvc',
+    function ($scope, $stateParams, baseFormData, $state, EnrolmentService, DictionariesSvc) {
       $scope.enrolmentId = $stateParams.id;
+
       $scope.entireEnrolment = {};
+      $scope.entireEnrolment.enrolmentsubjects = [];
+      $scope.entireEnrolment.benefits = [];
+      $scope.entireEnrolment.enrolment = {};
 
       $scope.enrolTabs = baseFormData.tabs;
 
@@ -17,5 +22,36 @@ angular
         } else {
           $state.go(route.new);
         }
+      };
+
+      $scope.brosweOrEditEnrolment = function (enrolmentId) {
+        EnrolmentService.getEntireEnrolment(enrolmentId).then(function (res) {
+          _.merge($scope.entireEnrolment.enrolmentsubjects, res.enrolmentsubjects);
+          _.merge($scope.entireEnrolment.benefits, res.benefits);
+          _.merge($scope.entireEnrolment.enrolment, res.enrolment);
+          _.merge($scope.entireEnrolment.statuses, res.statuses);
+
+        });
+      };
+
+      if ($stateParams.id) {
+        $scope.brosweOrEditEnrolment($stateParams.id);
+      } else {
+        EnrolmentService.clearCopy();
+      }
+
+      $scope.sendToServer = function (entireEnrolment) {
+        $scope.entireEnrolment.enrolment.note = 'some note';
+        EnrolmentService.addOrEditEnrolment(entireEnrolment).then(function () {
+          DictionariesSvc.clearStorageByRoute('enrolments');
+          $state.go ('root.enrolment.list');
+        });
+
+        $scope.delete = function () {
+          EnrolmentService.deleteEntireEnrolment().then(function () {
+            DictionariesSvc.clearStorageByRoute('enrolments');
+            $state.go ('root.enrolment.list');
+          });
+        };
       };
     }]);
