f1cd1f37e7464b2a15be12a7e4231fa426e403ba
kmuzytc <kostya.muzychenko@gmail.com>
2015-05-14 16:40:07 +0300

    commit

---
+++ app/scripts/controllers/enrolments/tabBenefits.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular.module('admissionSystemApp')
+  .controller('TabBenefitsCtrlEnrolment', ['$scope', '$location', 'SpecoffersService', 'BenefitsSvc',
+    function ($scope, $location, SpecoffersService, BenefitsSvc) {
+      $scope.entireEnrolment.benefits = [];
+      $scope.obj = {};
+      $scope.obj.selectedBenefit = [];
+
+      SpecoffersService.getEntireSpecoffer(826).then(function (specoffer) {
+
+        $scope.benefitsIDs = angular.copy (specoffer.benefits);
+        console.log ($scope.benefitsIDs);
+
+        BenefitsSvc.getBenefits().then(function (data) {
+          var array1 = angular.copy(data.benefitsArray),
+              array2 = angular.copy(data.benefitsMainArray),
+              x = 0,
+              y;
+
+          $scope.benefitsName = array1.concat(array2);
+          $scope.renderSelectArray = [];
+
+          for (x; x < $scope.benefitsIDs.length; x++) {
+            y = 0;
+            for (y; y < $scope.benefitsName.length; y++) {
+              if ($scope.benefitsIDs[x].benefitId === $scope.benefitsName[y].id) {
+                $scope.renderSelectArray.push($scope.benefitsName[y]);
+                break;
+              }
+            }
+          }
+
+          $scope.$watch ('entireEnrolment.benefits.length', function () {
+            var xx = 0,
+                yy;
+
+            $scope.renderTableArray = [];
+
+            for (xx; xx < $scope.entireEnrolment.benefits.length; xx++) {
+              yy = 0;
+              for (yy; yy < $scope.benefitsName.length; yy++) {
+                if ($scope.entireEnrolment.benefits[xx].benefitId === $scope.benefitsName[yy].id) {
+                  $scope.renderTableArray.push($scope.benefitsName[yy]);
+                  break;
+                }
+              }
+            }
+          });
+        });
+      });
+
+      $scope.insert = function () {
+        $scope.entireEnrolment.benefits.push ({
+          benefitId : $scope.obj.selectedBenefit.id, note: ''
+        });
+        $scope.obj.selectedBenefit = undefined;
+      };
+
+      $scope.removeRow = function (id) {
+        var index = -1,
+          comArr = $scope.renderTableArray,
+          i = 0,
+          x = 0;
+
+        for (i; i < comArr.length; i++) {
+          if (comArr[i].id === id) {
+            index = i;
+            break;
+          }
+        }
+
+        for (x; x < $scope.entireEnrolment.benefits.length; x++) {
+          if ($scope.entireEnrolment.benefits[x].benefitId === id) {
+            $scope.entireEnrolment.benefits.splice(x, 1);
+            break;
+          }
+        }
+
+        $scope.renderTableArray.splice(index, 1);
+      };
+    }
+  ]);
--- app/scripts/controllers/specoffer/tabBenefit.js
+++ app/scripts/controllers/specoffer/tabBenefit.js
@@ -1,97 +1,101 @@
 'use strict';
 
-angular.module('admissionSystemApp')
+angular
+  .module('admissionSystemApp')
+  .controller('TabBenefitCtrl', ['$scope', '$modal', '$state', 'BenefitsSvc',
+    function ($scope, $modal, $state, BenefitsSvc) {
 
+      //Internal object for rendering data in table
+      $scope.everything = {};
+      $scope.everything.allInformationArray = [];
 
-  .controller('TabBenefitCtrl', ['$scope', '$modal', '$state', 'BenefitsSvc', function ($scope, $modal, $state, BenefitsSvc) {
-
-    //Internal object for rendering data in table
-    $scope.everything = {};
-    $scope.everything.allInformationArray = [];
-
-    //Ng-model for modalBenefit.html
-    $scope.allBenefits = {};
-    $scope.allBenefits.benefit = '';
-
-    //Get data from server
-    if ($state.is('root.specoffer.new.benefit')) {
+      //Ng-model for modalBenefit.html
+      $scope.allBenefits = {};
+      $scope.allBenefits.benefit = '';
 
       //Get data from server
-      BenefitsSvc.getBenefits().then(function (data) {
-        $scope.benefits = data.benefitsArray;
-        $scope.everything.allInformationArray = angular.copy(data.benefitsMainArray);
+      if ($state.is('root.specoffer.new.benefit')) {
+        var y = 0;
 
-        for (var y = 0; y < $scope.everything.allInformationArray.length; y++) {
-          $scope.entireSpecoffer.benefits.push({benefitId: $scope.everything.allInformationArray[y].id, note: ''});
-        }
-      });
-    }
-    else {
-      BenefitsSvc.getBenefits().then(function (data) {
-        var array1 = angular.copy(data.benefitsArray);
-        var array2 = angular.copy(data.benefitsMainArray);
-        $scope.benefits = array1.concat(array2);
+        //Get data from server
+        BenefitsSvc.getBenefits().then(function (data) {
+          $scope.benefits = data.benefitsArray;
+          $scope.everything.allInformationArray = angular.copy(data.benefitsMainArray);
 
-        //Render data from server to table
-        $scope.$watch('entireSpecoffer.benefits.length', function () {
-          $scope.everything.allInformationArray = [];
-          for (var i = 0; i < $scope.entireSpecoffer.benefits.length; i++) {
-            for (var x = 0; x < $scope.benefits.length; x++) {
-              if ($scope.entireSpecoffer.benefits[i].benefitId === $scope.benefits[x].id) {
-                $scope.everything.allInformationArray.push($scope.benefits[x]);
-                break;
+          for (y; y < $scope.everything.allInformationArray.length; y++) {
+            $scope.entireSpecoffer.benefits.push({
+                benefitId: $scope.everything.allInformationArray[y].id, note: ''
+              });
+          }
+        });
+      }
+      else {
+        BenefitsSvc.getBenefits().then(function (data) {
+          var array1 = angular.copy(data.benefitsArray),
+            array2 = angular.copy(data.benefitsMainArray),
+            i = 0,
+            x = 0;
+
+          $scope.benefits = array1.concat(array2);
+
+          //Render data from server to table
+          $scope.$watch('entireSpecoffer.benefits.length', function () {
+            $scope.everything.allInformationArray = [];
+            for (i; i < $scope.entireSpecoffer.benefits.length; i++) {
+              for (x; x < $scope.benefits.length; x++) {
+                if ($scope.entireSpecoffer.benefits[i].benefitId === $scope.benefits[x].id) {
+                  $scope.everything.allInformationArray.push($scope.benefits[x]);
+                  break;
+                }
               }
             }
+          });
+        });
+      }
+
+      //Function opens modal window
+      $scope.open = function (size) {
+        $modal.open({
+          templateUrl: '../views/modal/modalBenefit.html',
+          scope: $scope,
+          controller: function ($scope, $modalInstance) {
+            $scope.ok = function () {
+              $scope.entireSpecoffer.benefits.push({
+                benefitId: $scope.allBenefits.benefit.id, note: ''
+              });
+              $scope.allBenefits.benefit = undefined;
+              $modalInstance.close();
+            };
+            $scope.cancel = function () {
+              $scope.allBenefits.benefit = undefined;
+              $modalInstance.dismiss('cancel');
+            };
+          },
+          size: size
+        });
+      };
+
+      //Remove data from table function
+      $scope.removeRow = function (id) {
+        var index = -1,
+            i = 0,
+            x = 0,
+            comArr = $scope.everything.allInformationArray;
+
+        for (i; i < comArr.length; i++) {
+          if (comArr[i].id === id) {
+            index = i;
+            break;
           }
-        })
-      });
+        }
+        for (x; x < $scope.entireSpecoffer.benefits.length; x++) {
+          if ($scope.entireSpecoffer.benefits[x].benefitId === id) {
+            $scope.entireSpecoffer.benefits.splice(x, 1);
+            break;
+          }
+        }
+        $scope.everything.allInformationArray.splice(index, 1);
+      };
+
     }
-
-    //Function opens modal window
-    $scope.open = function (size) {
-
-      $modal.open({
-        templateUrl: '../views/modal/modalBenefit.html',
-        scope: $scope,
-        controller: function ($scope, $modalInstance) {
-
-          $scope.ok = function () {
-            $scope.entireSpecoffer.benefits.push({benefitId: $scope.allBenefits.benefit.id, note:''});
-            $scope.allBenefits.benefit = undefined;
-            $modalInstance.close();
-          };
-
-          $scope.cancel = function () {
-            $scope.allBenefits.benefit = undefined;
-            $modalInstance.dismiss('cancel');
-          };
-
-        },
-        size: size
-      });
-    };
-
-    //Remove data from table function
-    $scope.removeRow = function(id){
-      var index = -1;
-      var comArr = $scope.everything.allInformationArray;
-      for( var i = 0; i < comArr.length; i++ ) {
-        if( comArr[i].id === id ) {
-          index = i;
-          break;
-        }
-      }
-
-      for( var x = 0; x < $scope.entireSpecoffer.benefits.length; x++ ) {
-        if( $scope.entireSpecoffer.benefits[x].benefitId === id ) {
-          $scope.entireSpecoffer.benefits.splice( x, 1 );
-          break;
-        }
-      }
-
-      $scope.everything.allInformationArray.splice( index, 1 );
-    };
-
-  }]);
-
-
+  ]);
