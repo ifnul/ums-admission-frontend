9f79587a12db74cbed2bed76659352bbd91fa275
Illya Mazyriak <illay1994@gmail.com>
2015-06-02 02:49:19 +0300

    change back end server

--- app/scripts/services/Constants.js
+++ app/scripts/services/Constants.js
@@ -2,7 +2,7 @@
 
 angular.module('admissionSystemApp')
   .constant('Constants', {
-    basicURL: 'http://176.36.11.25/api-lnu/',
+    basicURL: 'http://194.44.198.222:8080/is-lnu-rest-api/api/',
     BasicAuth: 'Basic YWRtaW46bmltZGE='
   })
   .constant('_', window._);
